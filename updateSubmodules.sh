87fcca5b3197a8be8c23cb21a54d7a5e781a3867
R3cursive <r3cursive@r3cursive.com>
2014-06-11 16:47:59 -0700

    bunch of functionality changes

--- updateSubmodules.sh
+++ updateSubmodules.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-for i in ./bundle/*; do
-    if [ -d "$i" ]; then
-        cd "$i" && git pull origin master
-    fi
-done
+#for i in ./bundle/*; do
+#    if [ -d "$i" ]; then
+#        cd "$i" && git pull origin master
+#    fi
+#done
+git submodule foreach git pull origin master
